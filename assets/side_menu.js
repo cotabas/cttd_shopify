fc095349826ecb8e8460528250387ba996e1bd9d
cotabas <cotabas@gmail.com>
2022-12-02 23:46:19 -0800

    fixed the max width issue on the catalog page and started working on the sort and filter menu

--- assets/side_menu.js
+++ assets/side_menu.js
@@ -3,6 +3,5 @@ const menu = window.document.getElementById("side_menu");
 
 function toggle_menu() {
   menu.style['visibility'] = menu.style['visibility'] == 'visible' ? 'hidden' : 'visible';
-  console.log(menu.style['visibility']);
 }
 fs_button.addEventListener("click", toggle_menu);
